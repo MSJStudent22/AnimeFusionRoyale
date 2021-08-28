87d057802ca220fa98e6e42dfd71221d88e86ba5
Mason Worthen <62263777+MSJStudent22@users.noreply.github.com>
2021-08-28 09:22:41 -0400

    Add files via upload

---
+++ client.js
@@ -0,0 +1,12 @@
+"use strict";
+var socket = io();
+//var username =prompt("Enter your name: ");
+
+//socket.emit("username",username);
+
+
+window.addEventListener("load",function (evt){
+
+
+});
+
